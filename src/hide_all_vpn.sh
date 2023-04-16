b94f5a6f16c0d9fe31725909c7b15fdd857a6adc
zeviel <77536370+zeviel@users.noreply.github.com>
2023-04-16 20:17:23 +0800

    Update hide_all_vpn.sh

--- src/hide_all_vpn.sh
+++ src/hide_all_vpn.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 first_api="https://plugin.hideallvpn.org"
+user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
 
 function create_user() {
 	curl --request POST \
 		--url "$first_api/misc/proxylistfree/action" \
-		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/x-www-form-urlencoded" \
 		--data "cmd=create&source=chrome"
 }
@@ -14,7 +15,7 @@ function get_password() {
 	# 1 - username: (string): <username>
 	curl --request POST \
 		--url "$first_api/misc/proxylistfree/action" \
-		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/x-www-form-urlencoded" \
 		--data "cmd=recover&user=$1&source=chrome"
 }
@@ -24,7 +25,7 @@ function login() {
 	# 2 - password: (string): <password>
 	curl --request POST \
 		--url "$first_api/misc/proxylistfree/action" \
-		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36" \
+		--user-agent "$user_agent" \
 		--header "content-type: application/x-www-form-urlencoded" \
 		--data "cmd=login&user=$1&pass=$2&md5=&source=chrome"
 }
